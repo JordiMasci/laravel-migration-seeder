2efb55a0aad4a1531591ab8630af8c3866f3a8fd
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-10-18 13:05:27 +0200

    feat

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HouseController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/train', [TrainController::class, 'index'])->name('train.index');
